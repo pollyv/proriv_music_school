bf289c7dd2b68c314d83a45466c70498ba1fecd9
Polina Zenova <polino4kaveselova@yandex.ru>
2024-05-15 19:31:28 +0300

    added header and banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,49 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          hot: true,
+          port: 8080,
+        },
+      };
 
-module.exports = {
-  entry: {
-    main: "./src/index.js",
-  },
+module.exports = ({ develop }) => ({
+  mode: develop ? "development" : "production",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "",
+    filename: "bundle.js",
+    clean: true,
   },
-  mode: "development",
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
-    open: true,
-    compress: true,
-    port: 8080,
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./styles/main.css",
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: "babel-loader",
-        exclude: "/node_modules/",
+        test: /\.(png|svg|jpg|jpeg)$/i,
+        type: "asset/inline",
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
-      },
-      {
-        test: /\.html$/,
+        test: /\.html$/i,
         loader: "html-loader",
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.scss$/,
+        test: /\.scss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
@@ -48,13 +52,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "./styles/main.css",
-    }),
-  ],
-};
+  ...devServer(develop),
+});
