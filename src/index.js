31d581fc53dc624d06ac300451603acb55feabe7
Polina Zenova <polino4kaveselova@yandex.ru>
2024-05-17 21:56:24 +0300

    feat: add slider with teachers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "./styles/main.scss";
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+const swiper = new Swiper(".slider-teacher", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
